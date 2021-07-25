9f01d1c3929f34e3d32ee029790519f6e58a7967
Ahmed Sadik Khan <ahmedsadikkhan@gmail.com>
2021-07-25 22:06:51 +0600

    calculator updated

--- 25.05.21 Homework/calculator.cpp
+++ 25.05.21 Homework/calculator.cpp
@@ -1,68 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
-float iSumFunc(int,int), iMinFunc(int,int),iMultFunc(int,int),iDivFunc(int,int);
-float chooseoper(int,int,char);
+float iSumFunc(float,float), iMinFunc(float,float),iMultFunc(float,float),iDivFunc(float,float);
+float chooseoper(float,float,char);
 int main()
 {
-    int num1,num2,result;
-    char oper;
+    string choice;
+    int rep=0;                                      //for (rep=0;rep=5;rep=rep+0)
+    while  (rep==0)
+    {
+        float num1,num2;
+        char oper;
 
-    cout<<"Please enter the first integer: "<<endl;
-    cin>>num1;
-    cout<<endl<<"Please enter any one of the following:\n"<<"+  -  *  /"<<endl<<endl;
-    cin>>oper;
-    cout<<endl<<"Please enter the second integer:"<<endl;
-    cin>>num2;
+        cout<<"Please enter the first number: "<<endl;
+        cin>>num1;
+        cout<<endl<<"Please enter any one of the following:\n"<<"+  -  *  /"<<endl<<endl;
+        cin>>oper;
+        cout<<endl<<"Please enter the second number:"<<endl;
+        cin>>num2;
 
-    cout<<endl<<"Result:\n";
-    cout<<num1<<oper<<num2<<"=";
-    chooseoper(num1,num2,oper);
+        cout<<endl<<"Result:\n";
+        cout<<num1<<" "<<oper<<" "<<num2<<" "<<"="<<" ";
+        chooseoper(num1,num2,oper);
+        cout<<endl;
+        cout<<"Enter \"1\" to repeat. Enter anything else to terminate the program."<<endl;
+        cin>>choice;
+        cout<<"------------------------------------------------------"<<endl;
+        if (choice!="1")
+        {
+            rep=1;
+        }
+        else
+        {
+            cout<<endl<<endl<<endl<<"------------------------------------------------------"<<endl;
+        }
+    }
+    cout<<"The program has been terminated.\nHave a beautiful day!"<<endl<<endl;
+    return 0;
+
 }
 
 
 
-float iSumFunc(int num1,int num2)
+float iSumFunc(float num1,float num2)
 {
     return (num1+num2);
 }
-float iMinFunc(int num1,int num2)
+float iMinFunc(float num1,float num2)
 {
     return (num1-num2);
 }
-float iMultFunc(int num1,int num2)
+float iMultFunc(float num1,float num2)
 {
     return (num1*num2);
 }
-float iDivFunc(int num1,int num2)
+float iDivFunc(float num1,float num2)
 {
     return (num1/num2);
 }
 
 
 
-float chooseoper(int num1,int num2,char oper)
+float chooseoper(float num1,float num2,char oper)
 {
     switch (oper)
     {
 
-        case '+':
-            cout<<iSumFunc(num1,num2);
+    case '+':
+        cout<<iSumFunc(num1,num2)<<endl;
         break;
 
-        case '-':
-            cout<<iMinFunc(num1,num2);
+    case '-':
+        cout<<iMinFunc(num1,num2)<<endl;
         break;
 
-        case '*':
-        cout<<iMultFunc(num1,num2);
+    case '*':
+        cout<<iMultFunc(num1,num2)<<endl;
         break;
 
-        case    '/':
-        cout<<iDivFunc(num1,num2);
+    case    '/':
+        cout<<iDivFunc(num1,num2)<<endl;
         break;
 
-        default:
-        cout<<"Error: wrong operator chosen.";
+    default:
+        cout<<"Error: wrong operator chosen."<<endl;
         break;
+        return 0;
     }
 }
