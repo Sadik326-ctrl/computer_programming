4c78a919924d17f7b44c236fc0b01ae334e04f5f
Ahmed Sadik Khan <ahmedsadikkhan@gmail.com>
2021-07-25 22:30:01 +0600

    01/ added

---
+++ 01/ex6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "\nThis is\t a string\n\t\t"
+" with \"many\" escape sequences!\n";
+
+cout<< endl << "How to print \\n?" << endl;
+
+cout << "Use double \\ i.e. \\\\ and n i.e. \\\\n .  Got it ?" << endl;
+return 0;
+}
